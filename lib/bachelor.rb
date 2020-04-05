2b32b509d67f617ef9c1f84fb2df2141ce5a92f9
austinckohler <austinckohler@gmail.com>
2020-04-05 11:28:53 -0600

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,58 @@
+require "pry"
+
+
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  first_name_of_season_winner = data[season].find { |contestant_name| contestant_name['status'] == "Winner" }
+  first_name_of_season_winner['name'].split.first
+end
+
+def all_contestants (data)
+  data.values.flatten
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  contestants = all_contestants(data)
+ contestants.each do |contestant|
+  if contestant["occupation"] == occupation
+    return name = contestant["name"]
+end
+end
 end
 
+
+  
 def count_contestants_by_hometown(data, hometown)
-  # code here
+ counter = 0
+ contestants = all_contestants(data)
+ contestants.each do |contestant|
+  if contestant["hometown"] == hometown
+    counter += 1
+#  binding.pry
 end
+end
+counter 
+end
+
+
 
 def get_occupation(data, hometown)
-  # code here
+  contestants = all_contestants(data)
+  contestants.map  do |contestant|
+    if contestant["hometown"] == hometown 
+      return contestant["occupation"]
+end
+end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  age = 0
+  counter = 0
+  data[season].each do |all_contestants|
+    all_contestants["age"] == "age"
+    counter += 1 
+    age += all_contestants["age"].to_f
+  
+end
+answer = (age/counter).round
 end
